f6e9b2e5868a40cf790eab7cf053dc3d6df19b4b
Umerocks <42742158+Umerocks@users.noreply.github.com>
2024-10-17 00:14:01 +0500

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -12,11 +12,11 @@ export default function Header(props: propsType){
     console.log(props)
     console.log(props.profile)
 
-    const [state, setState] = useState(0) // variable short hand state writing method... This is unchangeable data called Imuteablity
+    //const [state, setState] = useState(0) // variable short hand state writing method... This is unchangeable data called Imuteablity
 
     return(
         <div>  
-            <button className="mt-4" onClick={()=> console.log("Click")}  > {props.profile} {setState}</button>
+            {/* <button className="mt-4" onClick={()=> console.log("Click")}  > {props.profile} {State}</button> */}
         </div>
     )
 }
